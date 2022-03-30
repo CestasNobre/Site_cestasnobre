24b7bf53930b74ddae9359bc7cef363cab81f53b
Cestas Nobre <101732648+CestasNobre@users.noreply.github.com>
2022-03-30 20:43:29 -0300

    melhora do js e css responsivo

--- slider.js
+++ slider.js
@@ -1,4 +1,42 @@
+var ciclo = 1
+        //Criando um intervalo para troca da imagem automático
+        setInterval(function () {
+            ciclo++
+            if (ciclo > 2) {
+                ciclo = 1
+            }
+            if (window.matchMedia('(max-width: 530px)').matches) {
+                carregaFoto("imagens/f"+ciclo+"_responsivo.jpg")
+            } else {
+                carregaFoto("imagens/f"+ciclo+".jpg")
+            }
+            
+        }, 4000)    
 
+        //Cria uma função de correspondencia
+        function slider(x) {
+            if (x.matches) {
+                carregaFoto("imagens/f1_responsivo.jpg");
+                document.documentElement.style.setProperty('--ct_cliente', '60%');
+            } else {
+                carregaFoto("imagens/f1.jpg");
+                document.documentElement.style.setProperty('--ct_cliente', '40%');
+            }
+            
+        }
+
+        function carregaFoto(foto) {
+            document.getElementById("moldura").style.backgroundImage="URL("+foto+")";    
+        }
+
+        //Cria um objeto MediaQueryList
+        const jv = window.matchMedia('(max-width: 530px)')
+        //Chama a função match em tempo de execução
+        slider(jv)
+        //Adiciona a função match como listener para mudanças de estado:
+        jv.addListener(slider)
+
+        /*
 function preload() {
     if (largura <= 530) {
         imgs = Array('imagens/f1_responsivo.jpg','imagens/f2_responsivo.jpg')
@@ -92,3 +130,4 @@ function troca(dr) {
     }
     tmr = setInterval(trocaFoto, 5000);
 }
+*/

5b2624b3490fd029ab1528893015008da9698db0
Cestas Nobre <101732648+CestasNobre@users.noreply.github.com>
2022-04-15 15:36:39 -0300

    ajustes

--- menu_responsivo.js
+++ menu_responsivo.js
@@ -1,14 +1,21 @@
 var interacaoMenu = 0;
+/*
+let icon = document.getElementById('icon_resp') 
+icon.setAttribute('src','imagens/icone_x_menu_resp.png')
+*/
 
 function abreFechaMenu() {
     let abertura_menu = document.getElementById('cx_nav')
-    
-    if(interacaoMenu == 0){ 
-        abertura_menu.style.display='block'
-        interacaoMenu = 1
-    }else {
-        abertura_menu.style.display='none'
-        interacaoMenu = 0
+    if (window.matchMedia('(max-width: 900px)').matches){
+        
+        if(interacaoMenu == 0){ 
+            abertura_menu.style.display='block'
+            document.getElementById('icon_resp').src='imagens/icone_x_menu_resp.png'
+            interacaoMenu = 1
+        }else {
+            abertura_menu.style.display='none'
+            document.getElementById('icon_resp').src='imagens/icone_menu_responsivo.png'
+            interacaoMenu = 0
+        }
     }
-    
 }

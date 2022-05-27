d19174887b3f41cab8fc488feea4df5f021ba1e3
Diego de souza <diegodesouza.souza@gmail.com>
2022-05-27 11:50:18 -0300

    dia dos namorados

--- slider.js
+++ slider.js
@@ -1,3 +1,24 @@
+
+function sliderMovel() {
+    let countImg = 0;
+    const containerExt = document.getElementById("moldura");
+    const arrayFoto = ['fundo_namorados_adulto', 'fundo_namorados_homossexual', 'fundo_namorados_senhores'];
+    containerExt.style.backgroundImage=`url('./imagens/dia_dos_namorados/fundo_namorados_adulto.jpg')`;
+
+    setInterval(function () {
+        countImg++
+        let mudaFoto = 0;
+        if(countImg == 3) {
+            countImg = 0;
+        }
+        mudaFoto = arrayFoto[countImg]
+        containerExt.style.backgroundImage=`url('./imagens/dia_dos_namorados/${mudaFoto}.jpg')`;
+        
+    },5000);
+   
+}
+
+/*
 var ciclo = 1
 
 
@@ -39,98 +60,4 @@ function carregaFoto(foto) {
         //Adiciona a função match como listener para mudanças de estado:
         jv.addListener(slider)
 
-        /*
-function preload() {
-    if (largura <= 530) {
-        imgs = Array('imagens/f1_responsivo.jpg','imagens/f2_responsivo.jpg')
-    } else {
-        imgs = Array('imagens/f1.jpg','imagens/f2.jpg')
-    }
-    ;
-    imgQtde = imgs.length;
-    for (imgs = 0; imgs < imgQtde; imgs++) {
-        var preloading = new Image();
-        preloading.src = imgs[1];
-    }
-}
-
-   var largura = window.innerWidth
-|| document.documentElement.clientWidth
-|| document.body.clientWidth;
-    var altura = window.innerHeight
-|| document.documentElement.clientHeight
-|| document.body.clientHeight;
-
-if (largura <= 560) {
-    document.documentElement.style.setProperty('--vp_slider', '280px');
-    document.documentElement.style.setProperty('--ct_cliente', '60%');
-    document.documentElement.style.setProperty('--ct_lt', '0.8em');
-    document.documentElement.style.setProperty('--msg_lt', '80%');
-} else {
-    document.documentElement.style.setProperty('--vp_slider', '550px');
-    document.documentElement.style.setProperty('--ct_cliente', '40%');
-    document.documentElement.style.setProperty('--ct_lt', '1.5em');
-    document.documentElement.style.setProperty('--msg_lt', '30%');
-}
-
-
-console.log(altura);
-console.log(largura);
-
-function iniciaSlider() {
-    preload();
-    max = 2;
-    min = 1;
-    fotoIndice = min;
-    tr = true;
-    if (largura <= 530) {
-        carregaFoto("imagens/f1_responsivo.jpg");
-    } else {
-        carregaFoto("imagens/f1.jpg");
-    }
-    document.getElementById("moldura").addEventListener("transitionend", fimTr);
-    tmr = setInterval(trocaFoto, 5000);
-}
-
-function trocaFoto() {
-    tr = false;
-    fotoIndice++;
-    if (fotoIndice > max) {
-        fotoIndice = min;
-    }
-    if (largura <= 530) {
-        carregaFoto("imagens/f"+fotoIndice+"_responsivo.jpg");
-    } else {
-        carregaFoto("imagens/f"+fotoIndice+".jpg");
-    }
-    
-}
-
-function fimTr() {
-    tr = true;
-}
-
-function carregaFoto(foto) {
-    document.getElementById("moldura").style.backgroundImage="URL("+foto+")";    
-}
-
-function troca(dr) {
-    clearInterval(tmr);
-    if (tr) {
-        tr = false;
-        fotoIndice+= dr ;
-        if (fotoIndice > max) {
-            fotoIndice = min;
-        }
-        if (fotoIndice < min) {
-            fotoIndice = max;
-        }
-        if (largura <= 530) {
-            carregaFoto("imagens/f"+fotoIndice+"_responsivo.jpg");
-        } else {
-            carregaFoto("imagens/f"+fotoIndice+".jpg");
-        }
-    }
-    tmr = setInterval(trocaFoto, 5000);
-}
 */
